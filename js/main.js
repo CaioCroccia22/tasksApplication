8b70e76b5296bd11cc1386640a8854381a834da1
caiocroccia22 <caiocroccia22@gmail.com>
2024-09-16 20:48:50 -0300

    feat: automatically add new task as HTML list item

--- js/main.js
+++ js/main.js
@@ -4,4 +4,18 @@ $('form button').click(function(event){
     const inputTime = $('#time').val();
     console.log(inputTask);
     console.log(inputTime);
+
+    const newElement = $('<li "display:none;" ></li>');
+    // console.log(newElement);
+    $(`<div> ${inputTask} </div>`).appendTo(newElement);
+    $(`<div> ${inputTime} hrs </div>`).appendTo(newElement);
+    $(`<div><button id="btn-cancel">Cancelar</button></div>`).appendTo(newElement);
+
+    
+    // console.log(newElement);
+    newElement.appendTo('ul');
+
+
+    console.log(newElement)
+
 })
